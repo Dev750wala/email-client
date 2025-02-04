e179cd8172118d424dc68dc420f7753103ecdec8
Dev750wala <sadisatsowaladev1@gmail.com>
2025-02-05 02:50:54 +0530

    for production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 
-import { LatestPost } from "@/app/_components/post";
-import { api, HydrateClient } from "@/trpc/server";
+// import { LatestPost } from "@/app/_components/post";
+// import { api, HydrateClient } from "@/trpc/server";
 import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+  // const hello = await api.post.hello({ text: "from tRPC" });
 
-  void api.post.getLatest.prefetch();
+  // void api.post.getLatest.prefetch();
 
   return (
-    <HydrateClient>
+    // <HydrateClient>
       <h1>Hello World</h1>
-    </HydrateClient>
+    // </HydrateClient>
   );
 }
