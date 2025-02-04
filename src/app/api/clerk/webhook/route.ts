315423d68d22f6baae902e3253a8e736ead3f1e3
Dev750wala <sadisatsowaladev1@gmail.com>
2025-02-05 02:20:04 +0530

    for push

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,8 +1,26 @@
 // api/clerk/webhook
 
-export const POST = async (req: Request, res: Response) => {
+import { db } from "@/server/db"
+
+export const POST = async (req: Request) => {
     const { data } = await req.json()
-    console.log("webhook data", data);
+    // console.log("webhook data", data);
+
+    const emailAddress = data.email_addresses[0].email_address
+    const firstname = data.first_name
+    const lastname = data.last_name
+    const imageUrl = data.image_url
+    const id = data.id
+    
+    await db.user.create({
+        data: {
+            id: id,
+            emailAddress: emailAddress,
+            firstName: firstname,
+            lastName: lastname,
+            imageUrl: imageUrl
+        }
+    })
 
     return new Response("ok", {status: 200})
     
