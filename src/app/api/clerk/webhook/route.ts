73b54e901da48560109e92e7b5753f9cf996b92b
Dev750wala <sadisatsowaladev1@gmail.com>
2025-02-05 02:57:10 +0530

    for production

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -2,15 +2,77 @@
 
 import { db } from "@/server/db"
 
+export interface RequestBody {
+    data:             Data;
+    event_attributes: EventAttributes;
+    object:           string;
+    timestamp:        number;
+    type:             string;
+}
+
+export interface Data {
+    birthday:                 string;
+    created_at:               number;
+    email_addresses:          EmailAddress[];
+    external_accounts:        any[];
+    external_id:              string;
+    first_name:               string;
+    gender:                   string;
+    id:                       string;
+    image_url:                string;
+    last_name:                string;
+    last_sign_in_at:          number;
+    object:                   string;
+    password_enabled:         boolean;
+    phone_numbers:            any[];
+    primary_email_address_id: string;
+    primary_phone_number_id:  null;
+    primary_web3_wallet_id:   null;
+    private_metadata:         Metadata;
+    profile_image_url:        string;
+    public_metadata:          Metadata;
+    two_factor_enabled:       boolean;
+    unsafe_metadata:          Metadata;
+    updated_at:               number;
+    username:                 null;
+    web3_wallets:             any[];
+}
+
+export interface EmailAddress {
+    email_address: string;
+    id:            string;
+    linked_to:     any[];
+    object:        string;
+    verification:  Verification;
+}
+
+export interface Verification {
+    status:   string;
+    strategy: string;
+}
+
+export interface Metadata {
+}
+
+export interface EventAttributes {
+    http_request: HTTPRequest;
+}
+
+export interface HTTPRequest {
+    client_ip:  string;
+    user_agent: string;
+}
+
 export const POST = async (req: Request) => {
-    const { data } = await req.json()
+    const req1 = await req.json()
+    const data: RequestBody = req1
     // console.log("webhook data", data);
 
-    const emailAddress = data.email_addresses[0].email_address
-    const firstname = data.first_name
-    const lastname = data.last_name
-    const imageUrl = data.image_url
-    const id = data.id
+    const emailAddress = data.data.email_addresses[0]?.email_address as string
+    const firstname = data.data.first_name
+    const lastname = data.data.last_name
+    const imageUrl = data.data.image_url
+    const id = data.data.id
     
     await db.user.create({
         data: {
