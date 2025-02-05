2f69ef33ceaf25ceceb55c89c9ef6c0a1d8ae006
Dev750wala <sadisatsowaladev1@gmail.com>
2025-02-06 04:57:14 +0530

    add initial sync functionality and account management types

--- src/app/api/initial-sync/route.ts
+++ src/app/api/initial-sync/route.ts
@@ -1,5 +1,19 @@
+import { Account } from "@/lib/Account";
+import { db } from "@/server/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async (req: NextRequest, res: NextResponse) => {
-    
+export const POST = async (req: NextRequest) => {
+    const { accountId, userId } = await req.json();
+
+    if(!accountId || !userId) return NextResponse.json({message: 'Invalid request'}, {status: 400});
+
+    const dbAccount = await db.account.findUnique({
+        where: {
+            id: accountId,
+            userId: userId
+        }
+    })
+    if(!dbAccount) return NextResponse.json({message: 'Account not found'}, {status: 404});
+
+    const account = new Account(dbAccount.accessToken)
 }
---
+++ src/lib/Account.ts
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { SyncResponse, SyncUpdatedResponse } from "./types";
+import { resolve } from "path";
+import { headers } from "next/headers";
+
+export class Account {
+    private token: string;
+
+    constructor(token: string) {
+        this.token = token;
+    }
+
+    private async startSync() {
+        const response = await axios.post<SyncResponse>("https://api.aurinko.io/v1/email/sync", {}, {
+            headers: {
+                Authorization: `Bearer ${this.token}`
+            },
+            params: {
+                daysWithin: 2,
+                body: 'html'
+            }
+        })
+        return response.data;
+    }
+
+    async getUpdatedEmails({deltaToken, pageToken}: {deltaToken?: string, pageToken?: string}) {
+        let params: Record<string, string> = {};
+        if(deltaToken) params.deltaToken = deltaToken;
+        if(pageToken) params.pageToken = pageToken;
+
+        const response = await axios.get<SyncUpdatedResponse>("https://api.aurinko.io/v1/email/sync/updated", {
+            headers: {
+                Authorization: `Bearer ${this.token}`
+            },
+            params
+        })
+        return response.data;
+    }
+
+    async performInitialSync() {
+        try {
+            let syncResponse = await this.startSync();
+            while (!syncResponse.ready) {
+                await new Promise(resolve => setTimeout(resolve, 1000))
+                syncResponse = await this.startSync();
+            }
+
+            // get the delta token
+            let storedDeltaToken: string = syncResponse.syncUpdatedToken;
+
+            let updatedResponse = await this.getUpdatedEmails({ deltaToken: storedDeltaToken })
+
+            if(updatedResponse.nextDeltaToken) {
+                // completed sync
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
